3823615cbb13e56bceb36bfc3678d4497f23f3ee
GolfNorth <golf155@yandex.ru>
2025-03-24 22:18:31 +0500

    Сцена прелоудера

---
+++ Assets/Code/Scripts/Game.SDK/Data/SceneSettings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.SceneManagement;
+
+namespace Game.SDK.Components
+{
+    /// <summary>
+    /// Параметры для загрузки сцены
+    /// </summary>
+    [Serializable]
+    public class SceneSettings
+    {
+        [SerializeField]
+        private string sceneName;
+
+        [SerializeField]
+        private AssetReference sceneRef;
+
+        public object Key => string.IsNullOrEmpty(sceneName) ? sceneRef : sceneName;
+
+        [field: SerializeField]
+        public LoadSceneMode LoadMode { get; private set; }
+
+        [field: SerializeField]
+        public bool ActivateOnLoad { get; private set; }
+    }
+}
---
+++ Assets/Code/Scripts/Preloader.cs
@@ -0,0 +1,37 @@
+﻿using Cysharp.Threading.Tasks;
+using Game.SDK.Components;
+using Game.SDK.Services.Interfaces;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using VContainer;
+
+namespace Game
+{
+    /// <summary>
+    /// Последовательность действий перед запуском игры
+    /// </summary>
+    public class Preloader : MonoBehaviour
+    {
+        [SerializeField]
+        private SceneSettings[] scenes;
+
+        private ISceneService _sceneService;
+
+        [Inject]
+        public void Construct(ISceneService sceneService)
+        {
+            _sceneService = sceneService;
+        }
+
+        private async void Start()
+        {
+            await Addressables.InitializeAsync();
+
+            var loadSceneTasks = scenes.Select(x => _sceneService.LoadSceneAsync(x.Key, x.LoadMode, x.ActivateOnLoad));
+
+            await UniTask.WhenAll(loadSceneTasks);
+
+            Destroy(this);
+        }
+    }
+}
